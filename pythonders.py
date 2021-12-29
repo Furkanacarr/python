18c55ab696febbe1b8f304335599e291b578ecf1
Furkanacarr <93096686+Furkanacarr@users.noreply.github.com>
2021-12-29 18:35:04 +0300

    Add files via upload

---
+++ pythonders.py
@@ -0,0 +1,8 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+yas = input('Yaşınız : ')
+if(int(yas)<18):
+      print("Yaşınız Ehliyet almak İçin Uygun Değil")
+else:
+      print("Yaşınız Ehliyet almak İçin Uygun")
+      
